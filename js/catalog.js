43cd6935d9518f076cd92c9168fe88f807c241e1
dmitry <dmitryabov2017@yandex.ru>
2020-10-19 10:49:09 +0300

    добавил модальный окна на главную страницу и страницу каталога

---
+++ js/catalog.js
@@ -0,0 +1,26 @@
+const btnOrder = document.querySelectorAll(".btns-close");
+const orderPopup = document.querySelector(".modal-order");
+const modalClose = orderPopup.querySelector(".modal-close");
+
+
+
+  btnOrder.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        orderPopup.classList.add("modal-show");
+    })
+  })
+
+
+  modalClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    orderPopup.classList.remove("modal-show");
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (orderPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        orderPopup.classList.remove("modal-show");
+      }
+    }
+  });
